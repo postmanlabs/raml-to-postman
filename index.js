8d3a34dca60f80c4cc0642eefe6b29c9725efb2e
shreys7 <shreyshah97@gmail.com>
2019-11-28 18:46:18 +0530

    Added index.js to have convert, validate and getOptions function, in alignment with other importers

---
+++ index.js
@@ -0,0 +1,73 @@
+var converter = require('./lib/convert'),
+    fs = require('fs');
+module.exports = {
+    getOptions: function() {
+        return [];
+    },
+    convert: function(input, options, callback) {
+        var success = (collection, environment) => {
+            return callback(null, {
+                result: true,
+                output: [
+                    {
+                        type: 'collection',
+                        data: collection
+                    },
+                    {
+                        type: 'environment',
+                        data: environment
+                    }
+                ]
+            });
+        },
+        failure = (error) => {
+            if(typeof error === 'string' && error.includes('cannot fetch')) {
+                return callback({
+                    result: false,
+                    reason: 'External references are not supported yet. ' + error
+                });
+            }
+            return callback({
+                result: false,
+                reason: error
+            });
+        };
+        if (input.type === 'file') {
+            data = fs.readFileSync(input.data).toString();
+            converter.parseString(data, success, failure);
+        }
+        else if(input.type === 'string') {
+            converter.parseString(input.data, success, failure)
+        }
+        else {
+            return callback({
+                result: false,
+                reason: 'input type: ' + input.type + ' is not valid'
+            });
+        }
+    },
+    validate: function(input) {
+        let data;
+        if (input.type === 'file') {
+            data = fs.readFileSync(input.data).toString();
+            data = data.trim();
+            if (data.startsWith('#%RAML 0.8')) {
+                return {result: true};
+            }
+            return { result: false };
+        }
+        else if(input.type === 'string') {
+            data = input.data.trim();
+            if (data.startsWith('#%RAML 0.8')) {
+                return {result: true};
+            }
+            return { result: false };
+        }
+        else {
+            return { 
+                result: false,
+                reason: 'input type: ' + input.type + ' is not valid'
+            };
+        }
+    }
+}
